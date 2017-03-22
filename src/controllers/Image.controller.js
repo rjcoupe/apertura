d26c76db6d23d1138e65550aee015fb5b1920e72
Richard Coupe <richard@vegasky.io>
2017-03-22 21:18:49 +0000

    Exif processing

--- src/controllers/Image.controller.js
+++ src/controllers/Image.controller.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
 const ImageModel = mongoose.model('images');
 
+const crypto = require('crypto');
+const exifImage = require('exif').ExifImage;
+const fs = require('fs');
+const tmpfile = require('tmpfile');
+
 function ImageController(app) {
   this.app = app;
 }
 
 ImageController.prototype.route = function() {
   this.app.get('/api/image/:id', this.getImageById.bind(this), this.renderData.bind(this));
+  this.app.post('/api/image/upload',
+    this.getImageMD5.bind(this),
+    this.checkForPreviousUpload.bind(this),
+    this.uploadImageToS3.bind(this),
+    this.storeImageMetaData.bind(this)
+  );
 };
 
 ImageController.prototype.getImageById = function(request, response, next) {
@@ -19,6 +30,55 @@ ImageController.prototype.getImageById = function(request, response, next) {
     }
     request.imageData = image;
     return next();
+  });
+};
+
+ImageController.prototype.getImageMD5 = function(request, response, next) {
+  const image = request.files.imageUpload.path;
+  fs.readFile(image, (error, data) => {
+    request.image = {
+      md5: crypto.createHash('md5').update(data).digest('hex')
+    };
+    return next();
+  });
+};
+
+ImageController.prototype.checkForPreviousUpload = function(request, response, next) {
+  ImageModel.findOne({ md5: request.image.md5 }, (error, image) => {
+    if (image) {
+      return response.status(400).send('File already uploaded');
+    }
+    return next();
+  });
+};
+
+ImageController.prototype.uploadImageToS3 = function(request, response, next) {
+
+};
+
+ImageController.prototype.storeImageMetaData = function(request, response, next) {
+  new exifImage({ image: request.files.imageUpload.path }, (error, exif) => {
+    request.image.exif = {
+      camera: {
+        make: exif.image.Make,
+        model: exif.image.Model,
+        orientation: exif.image.Orientation,
+        xResolution: exif.image.XResolution,
+        yResolution: exif.image.YResolution,
+        resolutionUnit: exif.image.ResolutionUnit
+      },
+      data: {
+        fNum: exif.exif.FNumber,
+        aperture: exif.exif.ApertureValue,
+        iso: exif.exif.iso,
+        creationDate: exif.exif.CreateDate,
+        shutterSpeed: exif.exif.ShutterSpeedValue,
+        flash: (exif.exif.Flash === 1),
+        focalLength: exif.exif.FocalLength,
+        imageWidth: exif.exif.ExifImageWidth,
+        imageHeight: exif.exif.ExifImageHeight
+      }
+    };
   });
 };
 
